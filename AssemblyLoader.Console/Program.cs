4ebf7afb72d1512370fe2af6904618617f97232c
snovik <snovikovv@gmail.com>
2020-02-20 19:31:29 +0300

    Resolve libraries and test function

--- AssemblyLoader.Console/Program.cs
+++ AssemblyLoader.Console/Program.cs
@@ -1,8 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Runtime.CompilerServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using AssemblyLoader.Console.Properties;
+using MathLibrary;
+using TextLibrary;
 
 namespace AssemblyLoader.Console
 {
@@ -10,6 +16,18 @@ namespace AssemblyLoader.Console
     {
         static void Main(string[] args)
         {
+            AppDomain.CurrentDomain.ResolveAssemblies<BinaryBlobs>();
+
+            Start();
+        }
+
+        public static void Start()
+        {
+            MathClass math = new MathClass();
+            System.Console.WriteLine(math.Addition(2, 3));
+
+            TextClass text = new TextClass();
+            System.Console.WriteLine(@"Hello World");
         }
     }
 }
