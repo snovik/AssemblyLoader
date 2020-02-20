60d6a5d540323e1bf40df7b8c96ac2e41172e0ea
snovik <snovikovv@gmail.com>
2020-02-20 19:17:32 +0300

    Add two test class

---
+++ MathLibrary/MathClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathLibrary
+{
+    public class MathClass
+    {
+        public int Addition(int one, int two)
+        {
+            return one + two;
+        }
+    }
+}
---
+++ TextLibrary/TextClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextLibrary
+{
+    public class TextClass
+    {
+        public string EchoString(string str)
+        {
+            return str;
+        }
+    }
+}
